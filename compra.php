45b57ebc6d74f53966484286fc43a80ce4016c42
loppra@plataformasinformaticas.com <loppra@plataformasinformaticas.com>
2015-08-25 06:42:05 -0600

    v 0.1.0

---
+++ compra.php
@@ -0,0 +1,289 @@
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Asomol - Productos</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+
+	</head>
+	<body class="left-sidebar">
+		<div id="page-wrapper">
+            <?php
+                include "menu.php";
+                include "comprusr.php";
+                include "conect.php";
+            ?>
+			<!-- Main -->
+				<article id="main">
+
+
+					<!-- One -->
+						<section class="wrapper style4 container">
+                            <div class="paginador">
+                                <nav>
+                                    <ul>
+                                        <li>
+                                            <a href="main.php">Main </a>
+                                        </li>
+                                        <li>
+                                            >>
+                                        </li>
+                                        <li>
+                                            <a href="compra.php"> Compras </a>
+                                        </li>
+                                        <?php
+                                            if(isset($_POST['btnEditar'])){
+                                        ?>
+                                                    <li>
+                                                     >>
+                                                    </li>
+                                                    <li>
+                                                        <strong>Editar</strong>
+                                                    </li>
+                                        <?php
+
+                                            }elseif(isset($_POST['btnDel'])){
+
+
+                                        ?>          <li>
+                                                     >>
+                                                    </li>
+                                                    <li>
+                                                        <strong>Eliminar</strong>
+                                                    </li>
+                                        <?php
+
+                                            }elseif(isset($_POST['btnAdd'])){
+                                         ?>
+                                                    <li>
+                                                     >>
+                                                    </li>
+                                                    <li>
+                                                        <strong>Añadir</strong>
+                                                    </li>
+                                        <?php
+                                            }
+                                        ?>
+
+                                    </ul>
+                                </nav>
+                            </div>
+							<div class="row 150%">
+								<div class="12u 12u(narrower)">
+
+									<!-- Sidebar -->
+										<div class="sidebar">
+
+											<section class="formulario">
+												<header>
+													<h3><strong>Compras</strong></h3>
+												</header>
+												<p>Aquí puedes ver las operaciones con Compras de productos, puedes ingresar tus compras de productos, eliminar y modificar.</p>
+												<p>
+                                                <?php
+                                                    //editar
+
+
+                                                    if(isset($_POST['btnEditar'])){
+                                                        if(isset($_POST['txtEditar'])){
+                                                            //guardar
+                                                            $sql="UPDATE `presentacionProducto` SET `descripcion`='".$_POST['txtEditar']."' WHERE `id`='".$_POST['btnEditar']."';";
+                                                            $con -> query($sql);
+                                                    ?>
+                                                    <a href="compra.php" class="button special">Datos Guardados. Regresar</a>
+                                                    <?php
+                                                        }else{
+                                                            //cargar formulario para modificar
+
+                                                        $sql="SELECT descripcion FROM presentacionProducto where id='".$_POST['btnEditar']."'";
+                                                        $resultado = $con -> query($sql);
+                                                        $obj = $resultado->fetch_object();
+                                                ?>
+                                                    <form name="frmEditar" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+                                                        <label>Descripción: </label>
+                                                        <input name="txtEditar" type="text" value="<?php echo $obj->descripcion ; ?>"/>
+                                                        <label>Tipo de Producto: </label>
+
+
+                                                        <select name="tipoProd">
+                        <?php foreach ($rows as $row) {
+                            echo '<option value="'.$row['id'].'">'.$row['descripcion'].'</option>';
+                        }?>
+                    </select>
+
+
+                                                        <input name="txtEditar" type="text" value="<?php echo $obj->descripcion ; ?>"/>
+                                                        <label>Presentacion de Producto: </label>
+                                                        <input name="txtEditar" type="text" value="<?php echo $obj->descripcion ; ?>"/>
+                                                        <input type="hidden" name="btnEditar" value="<?php echo $_POST['btnEditar'];?>">
+                                                        <a href="compra.php" class="button special">Regresar</a>
+                                                        <input class="special" value="Guardar" type="submit" >
+                                                    </form>
+                                                <?php
+                                                        }
+                                                    }elseif(isset($_POST['btnDel'])){
+                                                        //eliminar
+                                                        if(isset($_POST['chbDel'])){
+                                                            //guardar
+                                                            $sql="DELETE FROM  `presentacionProducto` WHERE `id`='".$_POST['btnDel']."';";
+                                                            $con -> query($sql);
+                                                    ?>
+                                                            <a href="compra.php" class="button special">Datos eliminados. Regresar</a>
+                                                    <?php
+                                                        }else{
+                                                            //cargar formulario para eliminar
+                                                            $sql="SELECT descripcion FROM presentacionProducto where id='".$_POST['btnDel']."'";
+                                                            $resultado = $con -> query($sql);
+                                                            $obj = $resultado->fetch_object();
+                                                    ?>
+                                                        <form name="frmEditar" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+                                                            <p>
+                                                                Si elimina este tipo de producto; eliminara tambien todos los productos, ventas, compras y transacciones asociadas a este, generando problemas con sus datos.
+                                                            </p>
+
+                                                            <input type="checkbox" name="chbDel" value="true">Esta seguro que desea eliminar este tipo de producto. <strong><?php echo $obj->descripcion ; ?></strong><br>
+                                                            <input type="hidden" name="btnDel" value="<?php echo $_POST['btnDel'];?>">
+                                                            <a href="compra.php" class="button special">Regresar</a>
+                                                            <input class="special" value="Guardar" type="submit" >
+
+                                                        </form>
+                                                    <?php
+                                                        }
+                                                        //añadir
+                                                    }elseif(isset($_POST['btnAdd'])){
+                                                        if(isset($_POST['txtTp'])){
+                                                            //guardar
+                                                            $sql="INSERT INTO `presentacionProducto` (`descripcion`) VALUES ('".$_POST['txtTp']."');";
+                                                            $con -> query($sql);
+
+                                                    ?>
+                                                            <a href="compra.php" class="button special">Datos Guardados. Regresar</a>
+                                                    <?php
+                                                        }else{
+                                                            //cargar formulario para añadir
+                                                            $sql = "select id, nit from Proveedores;";
+                                                            if($resultado = $con -> query($sql)){
+
+                                                    ?>
+                                                    <form name="frmaddCompra" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+                                                        <label>Nit Proveedor: </label>
+                                                        <input list="nits" name="lsProv" required>
+                                                            <datalist id="nits">
+                                                                <?php
+                                                                while($obj = $resultado->fetch_object()){
+
+                                                                ?>
+                                                                <option value="<?php echo $obj->nit ; ?>">
+                                                                <?php
+                                                                }
+                                                                ?>
+
+                                                        </datalist>
+
+                                                        <input type="hidden" name="btnAdd" value="Añadir">
+                                                        <a href="compra.php" class="button special">Regresar</a>
+                                                        <input class="special" value="Guardar" type="submit" >
+                                                    </form>
+                                                <?php
+                                                            }
+                                                        }
+                                                    }else{
+                                                ?>
+                                                <form name="frmAdd" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+                                                    <input name="btnAdd" class="special" value="Añadir Nuevo" type="submit" >
+                                                </form> <br>
+												<table border="1" style="width:auto">
+													<tr>
+														<th>Id</th>
+														<th>Número de Factura</th>
+														<th>Fecha</th>
+                                                        <th>Nombre del Proveedor</th>
+                                                        <th>Dirección</th>
+                                                        <th>NIT</th>
+                                                        <th>Total</th>
+                                                        <th  colspan="2">Tareas</th>
+													</tr>
+                                                    <?php
+                                                        //
+                                                        $sql="SELECT Compra.id as id, Compra.facN as facN ,  fecha, Proveedores.nombre as nombre, Proveedores.direccion as direccion, Proveedores.nit as nit, Compra.total as total FROM Compra inner join Proveedores on Proveedores.id = Compra.Proveedores_id;";
+                                                        if($resultado = $con -> query($sql)){
+                                                            while($obj = $resultado->fetch_object()){
+                                                        ?>
+                                                    <tr>
+    													<td><?php echo $obj->id ; ?></td>
+    													<td><?php echo $obj->facN ; ?></td>
+                                                        <td><?php echo $obj->fecha ; ?></td>
+                                                        <td><?php echo $obj->nombre ; ?></td>
+                                                        <td><?php echo $obj->direccion ; ?></td>
+                                                        <td><?php echo $obj->nit ; ?></td>
+                                                        <td><?php echo $obj->total ; ?></td>
+
+														<td>
+															<form action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+                                                                <Button name="btnEditar" type="submit" value="<?php echo $obj->id; ?>">  Editar</button>
+                                                            </form>
+                                                        </td>
+
+														<td><form action="<?php echo $_SERVER['PHP_SELF'] ;?>" method="post">
+                                                                <Button name="btnDel" type="submit" value="<?php echo $obj->id; ?>" >Eliminar</button>
+                                                            </form>
+                                                        </td>
+  													</tr>
+                                                                <?php
+                                                            }
+                                                        }
+                                                    ?>
+
+
+												</table>
+                                                <?php }?>
+												</p>
+
+											</section>
+
+										</div>
+
+								</div>
+
+
+							</div>
+						</section>
+
+
+
+			<!-- Footer -->
+				<footer id="footer">
+
+
+
+					<ul class="copyright">
+						<li>&copy; Plataformas Informáticas</li><li> Web Application <a href="http://www.plataformasinformaticas.com">PI.com</a></li>
+					</ul>
+
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+
+	</body>
+</html>
