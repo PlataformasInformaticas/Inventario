6e778f6bb3f23b5d182276c07320f430512ab5ba
loppra@plataformasinformaticas.com <loppra@plataformasinformaticas.com>
2015-08-26 22:38:41 -0600

    V 0.1.3

--- compra.php
+++ compra.php
@@ -88,7 +88,7 @@
 													<h3><strong>Compras</strong></h3>
 												</header>
 												<p>Aquí puedes ver las operaciones con Compras de productos, puedes ingresar tus compras de productos, eliminar y modificar.</p>
-												<p>
+
                                                 <?php
                                                     //editar
 
@@ -178,54 +178,282 @@
                                                                     $idCompra = $_POST['txtId'];
                                                                 }
                                                             }
-                                                            //editar Detalle Compra
-                                                            if(isset($_POST['editarDescCompra'])){
-                                                            }elseif(isset($_POST['EliminarDescCompra'])){ //Eliminar
-                                                            }else{
-                                                ?>
-                                                <form name="frmaddDescCompra" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
-                                                    <input type="hidden" name="btnAdd" value="Añadir">
-                                                    <input type="hidden" name="lsProv" value="Producto">
-                                                    <input type="hidden" name="txtId" value="<?php  echo $idCompra?>">
-                                                    <label>Tipo de Producto</label>
-                                                    <label class="custom-select">
-                                                        <select name="slcTPDC" id="slcTPDC" onchange="CargarPresentacion(this.value);" required>
-                                                            <option disabled selected value="0">Seleccione un Valor</option>
+
+//Añadir Compra
+if(isset($_POST['btnSubDC'])){
+	$sql="INSERT INTO `descCompra` (`cantidad`, `precio`, `precioVenta`, `Compra_id`, `Producto_id` )
+	VALUES (".$_POST['txtCant'].", ".$_POST['txtPC'].", ".$_POST['txtPV'] .", ".$_POST['txtId'].", ".$_POST['slcPDC']." );";
+
+        $con -> query($sql);
+
+?>
+<h3>
+	Dato Añadido Correctamente
+</h3>
+<form name="frmaddDescCompra" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+	<input type="hidden" name="btnAdd" value="Añadir">
+	<input type="hidden" name="lsProv" value="Producto">
+	<input type="hidden" name="txtId" value="<?php  echo $_POST['txtId'];?>">
+	<input name="showFRMdet" class="special" value="Regresar" type="submit" >
+</form>
+<?php
+//editar Detalle Compra
+}elseif(isset($_POST['editarDescCompra'])){
+	if(isset($_POST['editarDescCompraFinish'])){
+        //Consulta para editar
+        $sql="UPDATE `descCompra` SET `cantidad`='".$_POST['txtCant']."', `precio`='".$_POST['txtPC']."', `precioVenta`='".$_POST['txtPV'] ."', `Producto_id`='".$_POST['slcPDC']."' WHERE `id`='".$_POST['editarDescCompra']."';";
+
+        echo $sql;
+        $con -> query($sql);
+
+?>
+<h3>
+	Dato Añadido Correctamente
+</h3>
+<form name="frmaddDescCompra" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+	<input type="hidden" name="btnAdd" value="Añadir">
+	<input type="hidden" name="lsProv" value="Producto">
+	<input type="hidden" name="txtId" value="<?php  echo $_POST['txtId'];?>">
+	<input name="showFRMdet" class="special" value="Regresar" type="submit" >
+</form>
+<?php
+	}else{
+		$sql="Select  descCompra.id as id,  descCompra.Producto_id  as Producto_id,  descCompra.cantidad as cantidad,
+		descCompra.precio as precio,  descCompra.precioVenta as precioVenta,  Producto.tipoProd_id as idtipoProd, Producto.presentacionProducto_id as idpresentacionProducto_id
+		From descCompra inner join Producto on Producto.id =  descCompra.Producto_id
+		where  descCompra.id= ". $_POST['editarDescCompra'];
+		$resultado = $con -> query($sql);
+		$obj2 = $resultado->fetch_object();
+?>
+<form name="frmEditarDetCompra" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+	<input type="hidden" name="btnAdd" value="Añadir">
+	<input type="hidden" name="lsProv" value="Producto">
+	<input type="hidden" name="editarDescCompra" value="<?php  echo $_POST['editarDescCompra']; ?>">
+	<input type="hidden" name="txtId" value="<?php  echo $_POST['txtId']; ?>">
+	<label>Tipo de Producto: </label>
+	<label class="custom-select">
+		<select name="slcTPDC" id="slcTPDC" onchange="CargarPresentacion(this.value,0);" required>
+			<option disabled value="0">Seleccione un Valor</option>
+			<?php
+		$sql = "SELECT tipoProd.id as id, tipoProd.descripcion as descripcion, Producto.id as prodid FROM Producto inner join tipoProd on tipoProd.id = Producto.tipoProd_id group by id order by descripcion asc;";
+		if($resultado = $con -> query($sql)){
+			while($obj = $resultado->fetch_object()){
+
+			?>
+
+			<option <?php if($obj->id==$obj2->idtipoProd){echo "selected"; $idtip=$obj->id;} ?>  value="<?php echo $obj->id ; ?>"><?php echo $obj->descripcion ; ?></option>
+			<?php
+			}
+		}
+			?>
+		</select>
+	</label>
+	<br>
+	<label>Presentación del Producto: </label>
+	<span id="PP">
+        <label class="custom-select">
+                                                        <select name="slcPPDC" id="slcPPDC" onchange="CargarProducto(this.value,0)" required>
+                                                            <option disabled value="0">Seleccione un Valor</option>
                                                             <?php
-                                                                $sql = "SELECT tipoProd.id as id, tipoProd.descripcion as descripcion FROM Producto inner join tipoProd on tipoProd.id = Producto.tipoProd_id group by id order by descripcion asc;";
+                                                                $sql = "SELECT presentacionProducto.id as id, presentacionProducto.descripcion as descripcion, Producto.id as prodid FROM Producto inner join presentacionProducto on presentacionProducto.id = Producto.presentacionProducto_id where Producto.tipoProd_id=".$idtip." group by id order by descripcion asc;";
+
                                                                 if($resultado = $con -> query($sql)){
                                                                     while($obj = $resultado->fetch_object()){
 
                                                             ?>
-                                                            <option  value="<?php echo $obj->id ; ?>"><?php echo $obj->descripcion ; ?></option>
+                                                            <option <?php if($obj->id==$obj2->idpresentacionProducto_id){echo "selected"; $idPP=$obj->id ;} ?>  value="<?php echo $obj->id ; ?>"><?php echo $obj->descripcion ; ?></option>
+                                                            <?php
+                                                                    }
+                                                                }
+
+                                                            ?>
+                                                        </select>
+                                                    </label>
+	</span><br>
+	<label>Producto: </label>
+	<span id="PROD">
+                                                    <label class="custom-select">
+                                                        <select name="slcPDC" id="slcPPDC" onchange="CargarPrecioProducto(this.value,0)" required>
+                                                            <option disabled  value="0">Seleccione un Valor</option>
+                                                            <?php
+                                                                $sql = "SELECT Producto.id as id, Producto.descripcion as descripcion FROM Producto  where Producto.tipoProd_id=".$idtip." and Producto.presentacionProducto_id=".$idPP." group by id order by descripcion asc;";
+                                                                if($resultado = $con -> query($sql)){
+                                                                    while($obj = $resultado->fetch_object()){
+
+                                                            ?>
+                                                            <option <?php if($obj->id==$obj2->Producto_id){echo "selected"; $idPP=$obj->id ;} ?>  value="<?php echo $obj->id ; ?>"><?php echo $obj->descripcion ; ?></option>
                                                             <?php
                                                                     }
                                                                 }
                                                             ?>
                                                         </select>
+
                                                     </label>
+
+	</span><br>
+	<label>Cantidad</label>
+	<input tipe="number" name="txtCant" placeholder="0" onchange="validarInt(this);" value="<?php echo $obj2->cantidad; ?>"  required >
+	<br>
+	<span id="Precios">
+                                                    <label>Precio Unitario de Compra: </label>
+													<input tipe="number" name="txtPC" required value="<?php echo $obj2->precio; ?>"
+														   onchange="validarDouble(this);" >
                                                     <br>
-                                                    <label>Presentación del Producto</label>
-                                                    <span id="PP">
-                                                    </span><br>
-                                                    <label>Producto</label>
-                                                    <span id="PROD">
-                                                    </span><br>
-                                                    <label>Cantidad</label>
-                                                    <br>
-                                                    <label>Precio de Compra</label>
-                                                    <br>
-                                                    <label>Precio de Venta</label>
-                                                    <br>
-                                                    <input id="btnSubDC" class="special" value="Guardar" type="submit" disabled>
-                                                </form>
-                                                            <a href="compra.php" class="button special">Datos Guardados. Regresar</a>
-                                                    <?php
-                                                            }
+                                                    <label>Precio Unitario de Venta: </label>
+													<input tipe="number" name="txtPV" required value="<?php echo $obj2->precioVenta; ?>"
+														   onchange="validarDouble(this);" >
+	</span>
+	<br>
+	<input id="btnSubDC" name="editarDescCompraFinish" class="special" value="Editar" type="submit" >
+</form>
+<!-- fin Editar Detalle Compra -->
+<?php
+	}
+}elseif(isset($_POST['EliminarDescCompra'])){ //Eliminar
+}else{
+?>
+<!--Formulario Descripcion de compra - Añadir -->
+<form name="frmaddDescCompra" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+	<input type="hidden" name="btnAdd" value="Añadir">
+	<input type="hidden" name="lsProv" value="Producto">
+	<input type="hidden" name="txtId" value="<?php  echo $idCompra;?>">
+	<label>Tipo de Producto: </label>
+	<label class="custom-select">
+		<select name="slcTPDC" id="slcTPDC" onchange="CargarPresentacion(this.value,0);" required>
+			<option disabled selected value="0">Seleccione un Valor</option>
+			<?php
+	$sql = "SELECT tipoProd.id as id, tipoProd.descripcion as descripcion FROM Producto inner join tipoProd on tipoProd.id = Producto.tipoProd_id group by id order by descripcion asc;";
+	  if($resultado = $con -> query($sql)){
+		  while($obj = $resultado->fetch_object()){
+
+			?>
+			<option  value="<?php echo $obj->id ; ?>"><?php echo $obj->descripcion ; ?></option>
+			<?php
+		  }
+	  }
+			?>
+		</select>
+	</label>
+	<br>
+	<label>Presentación del Producto: </label>
+	<span id="PP">
+	</span><br>
+	<label>Producto: </label>
+	<span id="PROD">
+	</span><br>
+	<label>Cantidad</label>
+	<input tipe="number" name="txtCant" placeholder="0.00" onchange="validarDouble(this);"  required >
+	<br>
+	<span id="Precios">
+	</span>
+	<br>
+	<input id="btnSubDC" name="btnSubDC" class="special" value="Añadir Producto" type="submit" disabled>
+	<a href="compra.php" class="button special">Regresar</a>
+</form>
+<!--Empiza tabla -->
+<br>
+<br>
+<h3>
+	Descripción de Factura
+</h3>
+<table border="1" style="width:auto">
+	<tr>
+		<th>Id</th>
+		<th>Número de Factura</th>
+		<th>Fecha</th>
+		<th>Nombre del Proveedor</th>
+		<th>Dirección</th>
+		<th>NIT</th>
+		<th>Total</th>
+	</tr>
+	<?php
+	  //
+	  $sql="SELECT Compra.id as id, Compra.facN as facN ,  fecha, Proveedores.nombre as nombre,
+	  Proveedores.direccion as direccion, Proveedores.nit as nit, Compra.total as total
+	  FROM Compra inner join Proveedores on Proveedores.id = Compra.Proveedores_id
+	  where Compra.id=" . $idCompra;
+	  if($resultado = $con -> query($sql)){
+		  while($obj = $resultado->fetch_object()){
+	?>
+	<tr>
+		<td><?php echo $obj->id ; ?></td>
+		<td><?php echo $obj->facN ; ?></td>
+		<td><?php echo $obj->fecha ; ?></td>
+		<td><?php echo $obj->nombre ; ?></td>
+		<td><?php echo $obj->direccion ; ?></td>
+		<td><?php echo $obj->nit ; ?></td>
+		<td><?php echo $obj->total ; ?></td>
+
+	</tr>
+	<?php
+		  }
+	  }
+	?>
 
 
-                                                    ?>
+</table>
+<br>
+<br>
+<h3>
+	Detalle de Factura
+</h3>
+<table border="1" style="width:auto">
+	<tr>
+		<th>Id</th>
+		<th>Descripción</th>
+		<th>Cantidad</th>
+		<th>Precio Unitario de Compra</th>
+		<th>Precio Unitario de Venta</th>
+		<th>Subtotal</th>
+                <th  colspan="2">Tareas</th>
+	</tr>
+	<?php
+	  //
+	  $sql="Select  descCompra.id as id,  Producto.descripcion as descripcion,  descCompra.cantidad as cantidad,
+	  descCompra.precio as precio,  descCompra.precioVenta as precioVenta,  (descCompra.cantidad *  descCompra.precio) as subtotal
+	  From descCompra inner join Producto on Producto.id =  descCompra.Producto_id
+	  where  descCompra.Compra_id= ". $idCompra;
 
+
+	  if($resultado = $con -> query($sql)){
+		  while($obj = $resultado->fetch_object()){
+	?>
+	<tr>
+		<td><?php echo $obj->id ; ?></td>
+		<td><?php echo $obj->descripcion ; ?></td>
+		<td><?php echo $obj->cantidad ; ?></td>
+		<td><?php echo $obj->precio ; ?></td>
+		<td><?php echo $obj->precioVenta ; ?></td>
+		<td><?php echo $obj->subtotal ; ?></td>
+
+		<td>
+
+			<form action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
+				<input type="hidden" name="btnAdd" value="Añadir">
+				<input type="hidden" name="lsProv" value="Producto">
+				<input type="hidden" name="txtId" value="<?php  echo $idCompra; ?>">
+				<Button name="editarDescCompra" type="submit" value="<?php echo $obj->id; ?>">  Editar</button>
+			</form>
+		</td>
+
+		<td>
+			<form action="<?php echo $_SERVER['PHP_SELF'] ;?>" method="post">
+				<input type="hidden" name="btnAdd" value="Añadir">
+				<input type="hidden" name="lsProv" value="Producto">
+				<input type="hidden" name="txtId" value="<?php  echo $idCompra;?>">
+				<Button name="EliminarDescCompra" type="submit" value="<?php echo $obj->id; ?>" >Eliminar</button>
+			</form>
+		</td>
+	</tr>
+	<?php
+		  }
+	  }
+	?>
+</table>
+<!--Termina Form de Detalle de Factura -->
+<?php
+	 }
+?>
                                                     <?php
                                                         }else{
                                                             //cargar formulario para añadir
@@ -249,11 +477,11 @@
                                                         	</datalist>
 														<br>
 														<label>Fecha: </label>
-														<input type="date" name="fecha" placeholder="aaaa-mm-dd" required> <br>
+														<input type="date" id="fecha" name="fecha" placeholder="aaaa-mm-dd" required onchange="validarFecha(this.value)"> <br>
 														<label>Número de Factura</label>
 														<input type="text" name="txtNfac" required>
 														<label>Total Facturado</label>
-														<input type="number" name="saldo" id="saldo"  step="any" >
+														<input type="number" name="saldo" id="saldo" required placeholder="0.00" onchange="validarDouble(this);"   step="any" >
 														<br>
                                                         <br>
                                                         <input type="hidden" name="btnAdd" value="Añadir">
@@ -313,7 +541,7 @@
 
 												</table>
                                                 <?php }?>
-												</p>
+
 
 											</section>
 
