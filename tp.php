4474cd008b0770b43dd02413d6c4718069346cfa
pankedos@gmail.com <pankedos@gmail.com>
2015-09-02 17:52:11 -0600

    v 0.1.7

--- tp.php
+++ tp.php
@@ -168,7 +168,7 @@
                                                 <form name="frmAdd" action="<?php echo $_SERVER['PHP_SELF'];  ?>" method="post">
                                                     <input name="btnAdd" class="special" value="Añadir Nuevo" type="submit" >
                                                 </form> <br>
-												<table border="1" style="width:auto">
+												<table class="rwd-table">
 													<tr>
 														<th>id</th>
 														<th>Descripción</th>
